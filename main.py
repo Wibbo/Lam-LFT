4ab09ed5305c90712c3fec45aad8a8c42c50a14c
Matthew Weaver <31797463+Wibbo@users.noreply.github.com>
2021-01-22 17:26:45 +0000

    General refactoring

--- main.py
+++ main.py
@@ -1,67 +1,89 @@
 import pandas as pd
-import numpy as np
 import csv
 import test_helper as th
 
-appt_list = []
+
+app_list = []
+
+with open('data/current.csv', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, skipinitialspace=True)
+
+    # Skip the header in the csv file.
+    next(csv_reader)
+
+    for row in csv_reader:
+        app_list.append(row)
+
+# Use comprehensions to replace blank fields and 'N/A' with No_Value.
+new_list = [[field or 'No_Value' for field in row] for row in app_list]
+new_list = [['No_Value' if field == 'N/A' else field for field in row] for row in new_list]
+
+with open('data/consolidated.csv', 'w', newline='') as f:
+    wr = csv.writer(f)
+    wr.writerow(['Office', 'Diary', 'Date', 'Start Time', 'Name',
+                 'Email Address', 'Telephone Number', 'Postcode',
+                 'Date of Birth', 'Reasons', 'Ethnicity', 'Notes'])
+    wr.writerows(new_list)
 
 
-# Read in appointment data from the corresponding data file.
-data_file = pd.read_csv('data/RawData-03.csv', encoding='utf8')
+data_file = pd.read_csv('data/consolidated.csv', encoding='latin1')
 data_file.sort_values(by=['Name', 'Postcode', 'Date', 'Start Time'], inplace=True, ascending=False)
+
 
 output_appt = ''
 previous_appt = ''
 
+
 def create_appt(appt_row):
-    appointment = [None] * 24
-    
-    appointment[0] = appt_row['Name']
-    appointment[1] = appt_row['Postcode']
-    appointment[2] = appt_row['Date of Birth']
-    appointment[3] = appt_row['Reasons']    
-    appointment[4] = appt_row['Ethnicity']   
-    appointment[5] = appt_row['Office']  
-    appointment[6] = appt_row['Date'] 
-    appointment[7] = th.get_test_outcome(appt_row['Notes']) 
+    appointment = [None] * 12
+    appointment[0] = appt_row['Office']
+    appointment[1] = appt_row['Diary']
+    appointment[2] = appt_row['Date']
+    appointment[3] = appt_row['Start Time']
+    appointment[4] = appt_row['Name']
+    appointment[5] = appt_row['Email Address']
+    appointment[6] = appt_row['Telephone Number']
+    appointment[7] = appt_row['Postcode']
+    appointment[8] = appt_row['Date of Birth']
+    appointment[9] = appt_row['Reasons']
+    appointment[10] = appt_row['Ethnicity']
+    appointment[11] = th.get_test_outcome(appt_row['Notes']) 
 
     return appointment
 
 
-def update_appt(appt_row, count, app):
+def update_appt(current_row, previous_row):
+    index = 0
+    for col_value in current_row:
+        if col_value is None:
+            col_value = previous_row[index]
+        index += 1
 
-    app[count + 8] = appt_row['Date']
-    app[count + 9] = th.get_test_outcome(appt_row['Notes'])
-
-    return app
+    return current_row
 
 
-field_offset = 0
 appt = None
 
+"""
 # Loop through every appointment.
 for index, row in data_file.iterrows():
 
-     #TODO Just use name for now.
+    #TODO Just use name for now.
     current_appt = row['Name'].lower()
  
     if current_appt != previous_appt:
         if previous_appt != '':
-            appt_list.append(appt) 
+            data_file.append(appt)
 
     if current_appt == previous_appt:
-        appt = update_appt(row, field_offset, appt)
-        field_offset += 2
+        appt = update_appt(row, appt)
     else:
         appt = create_appt(row)
-        field_offset = 0
     
     previous_appt = current_appt
+"""
 
-
-with open('data/consolidated.csv', 'w', newline='') as f:
-     wr = csv.writer(f)
-     wr.writerows(appt_list)
 
 
 
